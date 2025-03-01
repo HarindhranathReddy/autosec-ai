aced0080db39648fab37417049e4672c7bf8192d
HarryN <54132211+HarindhranathReddy@users.noreply.github.com>
2025-03-01 06:16:56 +0530

    Update recon.sh

--- modules/reconnaissance/recon.sh
+++ modules/reconnaissance/recon.sh
@@ -13,14 +13,12 @@ mkdir -p $output_directory
 # Run reconnaissance tools
 tools=(
     "subfinder -d $domain -o $output_directory/recon_subfinder.txt"
-    "amass enum -d $domain -o $output_directory/recon_amass.txt"
     "assetfinder --subs-only $domain > $output_directory/recon_assetfinder.txt"
-    "shuffledns -d $domain -w $output_directory/recon_amass.txt -r resolvers.txt -o $output_directory/recon_shuffledns.txt"
+    "shuffledns -d $domain -w $output_directory/recon_assetfinder.txt -r resolvers.txt -o $output_directory/recon_shuffledns.txt"
 )
 
 for tool in "${tools[@]}"; do
-    echo -e "\033[1;32mRunning: $tool\033[0m"
-    eval $tool
+    run_tool "$tool"
 done
 
 echo -e "\033[1;34m🔍 Reconnaissance completed for domain: $domain\033[0m"
